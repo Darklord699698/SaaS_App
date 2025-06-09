f7b20b1f870430966f5e056812162d12f9bd073e
UJJWAL <ujjwal8851114020@gmail.com>
2025-06-10 03:04:23 +0530

    companions cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import CompanionCard from '../components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my saas app</h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard/>
+        <CompanionCard/>
+        <CompanionCard/>
+      </section>
+      <section className='home-section'>
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
