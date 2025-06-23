7f559a4c45b9d86d66ad981abe0d131cb11cb8a7
UJJWAL <ujjwal8851114020@gmail.com>
2025-06-23 11:23:29 +0530

    appear

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
       <h1 className='text-2xl underline'>Popular Companions</h1>
       <section className='home-section'>
         <CompanionCard id="123" name="Neura the brainy explorer" topic="Derivatives and Integrals" subject="science" duration={45} color="#ffda6e" />
-        <CompanionCard id="456" name="Countsy the number wizard" topic="Derivatives and Integrals" subject="science" duration={30} color="#e5d0ff" />
+        <CompanionCard id="456" name="Countsy the number wizard" topic="Derivatives and Integrals" subject="Maths" duration={30} color="#e5d0ff" />
         <CompanionCard id="789" name="Verba the vocabulary builder" topic="language" subject="English Literature" duration={45} color="#BDE7FF" />
       </section>
       <section className='home-section'>
